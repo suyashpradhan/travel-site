9ed23aa8e9b0fcb0495f4ee0c1ee9626d4f515a6
Heisenberg017 <suyash9211@gmail.com>
2019-12-02 16:09:26 +0530

    Design: Design Completed.

--- script/script.js
+++ script/script.js
@@ -28,3 +28,66 @@ $(function () {
 
     var accordion = new Accordion($('#accordion'), false);
 });
+
+
+/*Pagination */
+let paginationLeftPos = "20px";
+let paginationOpacity = 0;
+let checkPaginationClick = 0;
+
+$(".pagination-page-number").click(function () {
+    $(".pagination-page-number").removeClass("active");
+    $(this).addClass("active");
+    paginationLeftPos = $(this).prop("offsetLeft") + "px";
+    paginationOpacity = 1;
+    checkPaginationClick = 1;
+
+    $(".pagination-hover-overlay").css({
+        left: paginationLeftPos,
+        backgroundColor: "#381805",
+        opacity: paginationOpacity
+    });
+    $(this).css({
+        color: "#fff"
+    });
+});
+
+$(".pagination-page-number").hover(
+    function () {
+        paginationOpacity = 1;
+        $(".pagination-hover-overlay").css({
+            backgroundColor: "#381805",
+            left: $(this).prop("offsetLeft") + "px",
+            opacity: paginationOpacity
+        });
+
+        $(".pagination-page-number.active").css({
+            color: "#333d45"
+        });
+
+        $(this).css({
+            color: "#fff"
+        });
+    },
+    function () {
+        if (checkPaginationClick) {
+            paginationOpacity = 1;
+        } else {
+            paginationOpacity = 0;
+        }
+
+        $(".pagination-hover-overlay").css({
+            backgroundColor: "#381805",
+            opacity: paginationOpacity,
+            left: paginationLeftPos
+        });
+
+        $(this).css({
+            color: "#381805"
+        });
+
+        $(".pagination-page-number.active").css({
+            color: "#fff"
+        });
+    }
+);
