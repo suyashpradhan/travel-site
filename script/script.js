dcae26f8a4afcea05b6124e53e22e5a6fa67d8c0
Heisenberg017 <suyash9211@gmail.com>
2020-01-09 16:33:12 +0530

    Design : Major Design Bug Fixes

--- script/script.js
+++ script/script.js
@@ -94,79 +94,22 @@ $(".pagination-page-number").hover(
 
 
 /*Navigation */
+function navbarFixed() {
+    if ($('.header_area').length) {
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+            if (scroll >= nav_offset_top) {
+                $(".header_area").addClass("navbar_fixed");
+            } else {
+                $(".header_area").removeClass("navbar_fixed");
+            }
+        });
+    };
+};
+navbarFixed();
 
-// NAVIGATION LOGO SCROLL TOP
-$('.logo').on('click', function (e) {
-    e.preventDefault();
-    $('.nav-toggle').removeClass('open');
-    $('.menu-left').removeClass('collapse');
-    $('html, body').animate({
-        scrollTop: 0
-    }, 750, 'easeInOutQuad')
+/*Select */
+$('select').niceSelect();
+$('#datetimepicker11,#datetimepicker1').datetimepicker({
+    daysOfWeekDisabled: [0, 6]
 });
-// LINKS TO ANCHORS
-$('a[href^="#"]').on('click', function (event) {
-
-    var $target = $(this.getAttribute('href'));
-
-    if ($target.length) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: $target.offset().top
-        }, 750, 'easeInOutQuad');
-    }
-});
-
-// TOGGLE HAMBURGER & COLLAPSE NAV
-$('.nav-toggle').on('click', function () {
-    $(this).toggleClass('open');
-    $('.menu-left').toggleClass('collapse');
-});
-// REMOVE X & COLLAPSE NAV ON ON CLICK
-$('.menu-left a').on('click', function () {
-    $('.nav-toggle').removeClass('open');
-    $('.menu-left').removeClass('collapse');
-});
-
-// SHOW/HIDE NAV
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function (event) {
-    didScroll = true;
-});
-
-setInterval(function () {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-        // Scroll Down
-        $('header').removeClass('show-nav').addClass('hide-nav');
-        $('.nav-toggle').removeClass('open');
-        $('.menu-left').removeClass('collapse');
-    } else {
-        // Scroll Up
-        if (st + $(window).height() < $(document).height()) {
-            $('header').removeClass('hide-nav').addClass('show-nav');
-        }
-    }
-
-    lastScrollTop = st;
-}
